3da0c9826ac044dd8f4a2b8826b9dfa3cebd4738
Anne Skou <anne-skou@hotmail.dk>
2021-02-03 17:17:07 +0100

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        private double sum = 0;
+        private double product = 0;
+        public double Add(double a, double b)
+        {
+            sum = a + b;
+            Console.WriteLine(sum);
+            return sum;
+        }
+        public double Subtract(double a, double b)
+        {
+            sum = a - b;
+            Console.WriteLine(sum);
+            return sum;
+        }
+        public double Multiply(double a, double b)
+        {
+            product = a * b;
+            Console.WriteLine(product);
+            return product;
+        }
+
+        public double Power(double x, double exp)
+        {
+            x = Math.Pow(x,exp);
+            Console.WriteLine(x);
+            return x;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Calculator calculator = new Calculator();
+            Console.WriteLine("a = 2 og b = 2");
+            calculator.Add(2, 2);
+            Console.WriteLine("a = 2 og b = 2");
+            calculator.Subtract(2, 2);
+            Console.WriteLine("a = 2 og b = 2");
+            calculator.Multiply(2, 2);
+            Console.WriteLine("a = 2 og b = 3");
+            calculator.Power(2, 3);
+        }
+    }
+}
